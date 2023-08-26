4adefe0693816983811a0eb136013d50b3d4a6d6
khansrfrz <khansarfaraz205@gmail.com>
2023-08-26 16:44:22 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , { useState , uuseEffect } from "react";
+import React , { useState , useEffect } from "react";
 import Loading from './Loading'
 import Tours from './Tours'
 
@@ -7,9 +7,56 @@ const url = 'https://course-api.com/react-tours-project'
 
 
 export default function App() {
+   const [loading,setLoading] = useState(true)
+   const [tours,setTours] = useState([])
+
+  const removeTour=(id)=>{
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try{
+      const response= await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    }catch(error){
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchTours()
+  },[])
+  
+  if(loading){
+    return(
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+if(tours.length===0){
+return(
+  <main>
+    <div className="title">
+      <h2>no tours left</h2>
+      <button className="btn" onClick={()=> fetchTours()}>
+        refresh
+      </button>
+    </div>
+  </main>
+)
+
+}
+
   return (
     <div>
-     Tours Project Setup      
+     <Tours tours={tours} removeTour={removeTour} />      
     </div>
   )
 }
+
